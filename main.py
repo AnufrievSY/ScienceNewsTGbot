ba76f5461d41f6e0f5d5ef547cc5cb42f3bb4f90
Sergey Anufriev <91721795+AnufrievSY@users.noreply.github.com>
2023-10-06 14:31:02 +0500

    Already existing developments have been added

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print('Hellow World!')
+from background import keep_alive 
+from scraper import get_df
+import telebot  
+import time
+from settings import TEST_BOT_TOKEN, CHAT_ID
+keep_alive()
+
+token = TEST_BOT_TOKEN
+bot = telebot.TeleBot(token)  
+
+chat_id = CHAT_ID
+i = 0
+while True:
+  df = get_df()
+  if df.empty:
+    pass
+  else:
+    # Текст сообщения
+    for i in df.index:
+  
+      message_text = '\n'.join([f"[{df.loc[i,'Название'].upper()}]({df.loc[i,'Ссылка']})",
+                          f"\n{' | '.join(df.loc[i,'Ключевые слова']+df.loc[i,'Науки']).lower()}\n",
+                          df.loc[i,'Краткое описание'],
+                          '\n'+', '.join(df.loc[i,'Авторы']),
+                          df.loc[i,'Дата публикации']])
+      bot.send_message(chat_id=chat_id, text=message_text, parse_mode="Markdown")
+  
+  time.sleep(5*60.0)
